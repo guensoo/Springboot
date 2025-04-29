5c720f5bd830604586605f1ed0e673a6783b384d
ghfkd12 <chbk9195@naver.com>
2025-04-29 17:38:59 +0900

    4. 29.(Tue) Spring study

---
+++ work/todo/src/main/java/com/korea/todo/model/TodoEntity.java
@@ -0,0 +1,17 @@
+package com.korea.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoEntity {
+	private String id; // 이 객체의 id
+	private String userId; // 이 객체를 생성한 유저의 아이디
+	private String title; // Todo의 타이틀
+	private boolean done; // Todo의 완료 여부
+}
