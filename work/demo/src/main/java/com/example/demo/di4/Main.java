69680cc273995dfe2b536cd517da03800143de65
ghfkd12 <chbk9195@naver.com>
2025-04-29 15:38:23 +0900

    4. 29.(Tue) Spring...Study...

--- work/demo/src/main/java/com/example/demo/di4/Main.java
+++ work/demo/src/main/java/com/example/demo/di4/Main.java
@@ -1,34 +1,67 @@
-package com.example.demo.di3;
+package com.example.demo.di4;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import com.google.common.reflect.ClassPath;
 
-// 컴포넌트 스캐닝
-// 클래스앞에 @Component 어노테이션을 붙이고
-// 패키지에 컴포넌트 어노테이션이 붙어있는 클래스를 찾아서
-// 객체로 만들고, 맵에 저장하는 기법
-
-
-@Component class Car{};
+@Component class Car{
+	@Autowired
+	Engine engine;
+	
+	@Autowired
+	Wheel wheel;
+	@Override
+	public String toString() {
+		return "Car [engine=" + engine+", wheel=" + wheel+"]";
+	}
+};
 @Component class SportCar extends Car{};
 @Component class Truck extends Car{};
 @Component class Engine{};
+@Component class Wheel{};
 
 class AppContext{
 	Map map;
 	public AppContext() {
 		map = new HashMap();
 		doComponentScan();
+		doAutowired();
+	}
+	
+	// map에 저장된 객체의 객체변수중 @Autowired가 붙어있으면
+	// 타입에 맞는 객체를 찾아서 연결한다.
+
+	private void doAutowired() {
+		try {
+			// 맵에 들어있는 객체를 하나씩 꺼내서
+			for(Object obj : map.values()) {
+				// getClass()로 클래스 정보를 얻어오고
+				// getDeclaredFields()를 통해서 해당 클래스에 있는 필드의 정보들을 배열로 반환
+				for(Field fld : obj.getClass().getDeclaredFields()) {
+					// 필드에 Autowired 어노테이션이 붙어있는지 확인하고,
+					// 붙어있으면 -> if
+					if(fld.getAnnotation(Autowired.class)!=null) {
+						// 그 필드에 맞는 객체가 있으면 세팅을 해라
+						fld.set(obj, getBean(fld.getType()));	
+					}
+					
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 	
 	// 패키지에 클래스를 모두 순회하면서 @Component 어노테이션이 붙은 클래스를
 	// Map에 객체로 등록을 한다.
+	// @ComponentScan이 아래 함수의 역할을 한다.
 	private void doComponentScan() {
 		try {
 			// 1. 패키지내의 클래스 목록을 가져온다.
@@ -47,7 +80,7 @@ class AppContext{
 			ClassPath classPath = ClassPath.from(classLoader);
 			
 			// 지정한 패키지 내의 최상위 클래스를 가져온다.
-			Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.example.demo.di3");
+			Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.example.demo.di4");
 			
 			for(ClassPath.ClassInfo classInfo : set) {
 				// ClassInfo 객체를 실제 Class로 변환을 한다.
@@ -99,17 +132,16 @@ public class Main {
 	public static void main(String[] args) {
 		AppContext ac = new AppContext();
 		
+		// car 객체에 필드로 engine과 wheel을 갖는다.
 		Car car = (Car)ac.getBean("car");
-		System.out.println("car = " + car);
-		
 		Engine engine = (Engine)ac.getBean("engine");
-		System.out.println("engine = "+engine);
+		Wheel wheel = (Wheel)ac.getBean("wheel");
 		
-		// 타입을 통해서 map에 저장되어 있는 객체 찾기
-		Car car2 = (Car)ac.getBean(Car.class);
-		System.out.println("car2 = "+car2);
+		// 원래 자바에서는 필드에 직접 객체를 넣어줘야한다.(의존성 주입)
+		// car.engine = engine;
+		// car.wheel = wheel;
 		
-		// 실제로는 @ComponentScan어노테이션으로 모든 과정을 퉁친다.
-		// 내부에서는 위와 같은 원리로 돌아가고 있다.
+		System.out.println(car);
+		
 	}
 }
